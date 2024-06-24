34a6c7e0777f66f64ff0dc3f6d9804d9638ec4a6
Bertolo <leticiab3rtolo@gmail.com>
2024-06-24 16:26:23 -0300

    projeto inicial

---
+++ js/app.js
@@ -0,0 +1,7 @@
+    function alterarStatus(){
+        let game1, game2, game3;
+        game1 = document.getElementById("game-1"); 
+        game2= document.getElementById("game-2"); 
+        game3 = document.getElementById("game-3"); 
+
+    }
