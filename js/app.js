10f67b8befd8a7f6d4c6e3ecbba204b43c3d2808
Bertolo <leticiab3rtolo@gmail.com>
2024-06-24 17:42:38 -0300

    projeto final

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
-    function alterarStatus(){
-        let game1, game2, game3;
-        game1 = document.getElementById("game-1"); 
-        game2= document.getElementById("game-2"); 
-        game3 = document.getElementById("game-3"); 
+    function alterarStatus(id){
+        let clicado  = document.getElementById(`game-${id}`); 
+        let img = clicado.querySelector('.dashboard__item__img'); 
+        let botao = clicado.querySelector('.dashboard__item__button');
+      
+      
+       if(img.classList.contains('dashboard__item__img--rented')){
+            img.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.innerHTML = "Alugar"
+        }else{
+            img.classList.add('dashboard__item__img--rented');
+            botao.innerHTML = "Devolver"
+            botao.classList.add('dashboard__item__button--return');
+        }
 
+      
     }
+
